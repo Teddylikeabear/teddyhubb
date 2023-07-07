99d510dc4c86acec1db793912676c408afe21498
Thendo Charmaine Marageni <132996253+Teddylikeabear@users.noreply.github.com>
2023-07-07 11:41:14 +0200

    myfirstpush

--- index.php
+++ index.php
@@ -32,4 +32,28 @@ $sql ="CREATE TABLE employees (
 
      }
 
+     //inserting data in the employees table 
+
+$sql = "INSERT INTO employees (empid , iinitials ,lastname ,age, email )
+VALUES('999 , TC , Marageni , 22 , tcmarageni@gmail.com')";
+
+$sql = "INSERT INTO employees (empid , iinitials ,lastname ,age, email )
+VALUES('998 , BS , Shabalala , 22 , bsshabalala@gmail.com')";
+
+$sql = "INSERT INTO employees (empid , iinitials ,lastname ,age, email )
+VALUES('997 , BI , Morakabi , 23 , bcmorakabi@gmail.com')";
+
+$sql = "INSERT INTO employees (empid , iinitials ,lastname ,age, email )
+VALUES('996 , TM , Mudau , 23 , tmmudau@gmail.com')";
+
+if ($conn->multi_query($sql)=== TRUE)
+{
+    echo "New records are created successfullly";
+} else 
+{
+    echo "Error : " .$sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
      ?>
