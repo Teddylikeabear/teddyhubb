549fa32c6a867f99abffdb515241f494646d334e
Thendo Charmaine Marageni <132996253+Teddylikeabear@users.noreply.github.com>
2023-07-13 11:59:21 +0200

    3rd code

--- selectdata.php
+++ selectdata.php
@@ -16,7 +16,13 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+/*$sql = "SELECT empid, initials, lastname FROM employees ";
+$result = $conn->query($sql);
+
 $sql = "SELECT empid, initials, lastname FROM employees WHERE lastname='Morakabi'";
+$result = $conn->query($sql);*/
+
+$sql = "SELECT empid, initials, lastname FROM employees ORDER BY lastname";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
@@ -24,10 +30,9 @@ if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
         echo "<br> Employee id: ". $row["empid"]. " - Name: ". $row["initials"]. " " . $row["lastname"] . "<br>";
-         
+        
     }
   
-
 } else {
     echo "0 results";
 }
