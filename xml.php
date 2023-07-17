8e772dfbcb76c3555e964fc0cd50f1edb804d76c
Thendo Charmaine Marageni <132996253+Teddylikeabear@users.noreply.github.com>
2023-07-17 13:55:03 +0200

    13pm17july

--- xml.php
+++ xml.php
@@ -70,6 +70,26 @@ function stop($parser,$element_name) {
 function char($parser,$data) {
     echo $data;
 }
+
+// Specify element handler
+xml_set_element_handler($parser,"start","stop");
+
+// Specify data handler
+xml_set_character_data_handler($parser,"char");
+
+// Open XML file
+$fp=fopen("note.xml","r");
+
+// Read data
+while ($data=fread($fp,4096)) {
+  xml_parse($parser,$data,feof($fp)) or
+  die (sprintf("XML Error: %s at line %d",
+  xml_error_string(xml_get_error_code($parser)),
+  xml_get_current_line_number($parser)));
+}
+
+// Free the XML parser
+xml_parser_free($parser);
 ?>
 
 </body>
