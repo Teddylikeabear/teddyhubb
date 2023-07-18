38bc371a5cc2e6505e451d522386f0c7bdd11a96
Thendo Charmaine Marageni <132996253+Teddylikeabear@users.noreply.github.com>
2023-07-18 13:59:36 +0200

    13pm18July

---
+++ ajmysql.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+table, td, th {
+  border: 1px solid black;
+  padding: 5px;
+}
+
+th {text-align: left;}
+</style>
+</head>
+<body>
+
+<?php
+$q = intval($_GET['q']);
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "dbteddy";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} 
+
+/*$con = mysqli_connect('localhost','teddydb','');
+if (!$con) {
+  die('Could not connect: ' . mysqli_error($con));
+}*/
+
+
+?>
+</body>
+</html>
