4f86afe190d55331ddf794a7a3c7f0f442c1fb08
Thendo Charmaine Marageni <132996253+Teddylikeabear@users.noreply.github.com>
2023-07-20 09:35:40 +0200

    9am-20July

---
+++ web/process.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Result</title>
+</head>
+<body>
+    <h2> Result</h2>
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $name = $_POST['name'];
+        $surname = $_POST['surname'];
+        $email = $_POST['email'];
+        $placement = $_POST['placement'];
+        $institution = $_POST['institution'];
+
+        echo "<p>Name: $name</p>";
+        echo "<p>Surname: $surname</p>";
+        echo "<p>Email: $email</p>";
+        echo "<p>Placement: $placement</p>";
+        echo "<p>Institution: $institution</p>";
+    
+    } else {
+        echo "<p>No data submitted.</p>";
+    }
+    ?>
+</body>
+</html>
