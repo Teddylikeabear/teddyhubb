f1407fe44fcdc4bb674b67db069001b86c04c83e
Thendo Charmaine Marageni <132996253+Teddylikeabear@users.noreply.github.com>
2023-07-28 10:13:19 +0200

    10am28July

--- teddy1/index.php
+++ teddy1/index.php
@@ -16,7 +16,8 @@ final class Index{
 
     //system config
 
-    function __constuct(){
+    function __constuct()
+    {
         //ERROR REPORTING DEVELOPMENT
         error_reporting(E_ALL | E_STRICT);
         mb_internal_encoding('UTF-8');
@@ -28,82 +29,106 @@ final class Index{
       //session_set_save_handler($handler)
      //session_start();
 
-}catch(Exception $e){
+    }
+      catch(Exception $e)
+    {
     throw $e;
-}
+    }
 
     }
 
     //CLASS LOADER
     public function loadclass($name)
     {
-        if (!array_key_exists($name, self ::$CLASS)){
+        if (!array_key_exists($name, self ::$CLASS))
+        {
             die('Class"'. $name .'" not found .');
 
         }
         require_once__DIR__.self :: $CLASS[$name];
     }
 
-   private function runPage($page, array $extra = []){
+   private function runPage($page, array $extra = [])
+   {
 
     $run = false;
-    if ($this -> hasScript($page)){
+       if ($this -> hasScript($page))
+       {
         $run = true;
         require $this->getScript($page);
-    }
-    if ($this-> hasTemplate($page)){
+       }
+       if ($this-> hasTemplate($page))
+       {
         $run = true;
         //data from main template 
         $template = $this -> getTemplate ($page);
         //main template (layout)
         require__DIR__.self::LAYOUT_DIR.self::LAYOUT_PAGE;
-    }
-    if (!$run){
+       }
+      if (!$run)
+      {
         die('Page "'.$page . '"has neither script nor template ! ');
 
-    }
+      }
 
-   }
- 
-   private function getPage(){
-    $page = self :: DEFAULT_PAGE;
-    if (array_key_exists('page',$_GET)){
-        $page = $_GET['page'];
     }
+ 
+    //gets pages
+   private function getPage()
+   {
+      $page = self :: DEFAULT_PAGE;
+      if (array_key_exists('page',$_GET))
+      {
+        $page = $_GET['page'];
+      }
     return $this -> checkPage($page);
    }
 
-   private function chechPage($page){
-    if (!preg_match('/^[a-z0-9-]+$/i',$page)){
+   //checks all pages
+
+   private function checkPage($page)
+   {
+       if (!preg_match('/^[a-z0-9-]+$/i',$page))
+       {
         //TODO LOG ATTEMPT , REDIRECT ATTACHER
         die('Unsafe page "'.$page . '" requested');
-    }
-    if (!$this -> hasScript($page)&& !$this -> hasTemplate($page)){
+       }
+       if (!$this -> hasScript($page)&& !$this -> hasTemplate($page))
+       {
         //TODO LOG ATTEMPTT , REDIRECT ATTACKER
         die('Page "' .$page . '" not found ');
-    }
+       }
     return $page;
    }
 
-   private function hasScript($page){
+   //checks php
+   private function hasScript($page)
+   {
     return file_exists($this -> getScript($page));
    }
 
-   private function getScript($page){
+   //gets php
+   private function getScript($page)
+   {
     return__DIR__.self :: PAGE_DIR.$page.'';
    }
 
-   private function hasTemplate($page){
+   //checks phtml
+   private function hasTemplate($page)
+   {
     return file_exists($this->getTemplate($page));
    }
 
-   private function getTemplate($page){
+   //gets phtml 
+   private function getTemplate($page)
+   {
     return__DIR__.self ::PAGE_DIR.$page.'';
    }
 
    //RUN APPLICATION
 
-   public function run(){
+   public function run() 
+   {
     $this -> runPage($this -> getPage());
    }
 }
