e9386f13ce24bc7e3ec1bfc2ab2b091745896e4a
Thendo Charmaine Marageni <132996253+Teddylikeabear@users.noreply.github.com>
2023-07-28 10:50:57 +0200

    calling function

--- teddy1/web/process.php
+++ teddy1/web/process.php
@@ -10,7 +10,11 @@ if ($_SERVER[ 'REQUEST_METHOD'] === 'GET')
     $user_placement = $_GET["placement"];
     $user_institution = $_GET["institution"];
 
-
+$user -> set_name ("$user_name");
+$user -> set_surname ("$user_surname");
+$user -> set_email ("$user_email");
+$user -> set_placement ("$user_placement");
+$user -> set_institution ("$user_institution");
     
 }
 
