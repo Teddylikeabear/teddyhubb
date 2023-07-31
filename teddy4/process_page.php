9b078986dbbb9bdcdd596197d465793264dc65d3
Thendo Charmaine Marageni <132996253+Teddylikeabear@users.noreply.github.com>
2023-07-31 12:04:00 +0200

    12:0031July

---
+++ teddy4/process_page.php
@@ -0,0 +1,18 @@
+<?php
+
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST")
+ {
+  $name = test_input($_POST["name"]);
+  $email = test_input($_POST["surname"]);
+ 
+ }
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
