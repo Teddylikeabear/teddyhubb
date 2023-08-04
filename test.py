5932b64e7ccca74957c9e837d8462691ac0739e0
Thendo Charmaine Marageni <132996253+Teddylikeabear@users.noreply.github.com>
2023-08-04 11:00:18 +0200

    11am04Aug

--- test.py
+++ test.py
@@ -178,3 +178,124 @@ a = """Hi my name is Thendo Charmaine Marageni.
 I am an aspiring data analyst."""
 print(a) 
 
+#Strings are Arrays
+a = "Hello, World!"
+print(a[1])
+
+#Looping Through a String
+for x in "banana":
+  print(x)
+
+#String Length
+a = "Hello, World!"
+print(len(a))
+
+#Check String
+txt = "The best things in life are free!"
+print("free" in txt)
+
+#if statement
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+#Check if NOT
+txt = "The best things in life are free!"
+print("expensive" not in txt) 
+
+#if statements
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
+
+#Slicing
+  b = "Hello, World!"
+print(b[2:5])
+
+#Slice From the Start
+b = "Hello, World!"
+print(b[:5])
+
+#Slice To the End
+b = "Hello, World!"
+print(b[2:])
+
+#Negative Indexing
+b = "Hello, World!"
+print(b[-5:-2])
+
+#Upper Case
+a = "Hello, World!"
+print(a.upper())
+
+#Lower Case
+a = "Hello, World!"
+print(a.lower())
+
+#Remove Whitespace
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!" 
+
+#Replace String
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+#Split String
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!'] 
+
+#String Concatenation
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+#To add a space between them, add a " ":
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+#String Format
+
+#Use the format() method to insert numbers into strings:
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
+
+#The format() method takes unlimited number of arguments, and are placed into the respective placeholders:
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {} pieces of item {} for {} dollars."
+print(myorder.format(quantity, itemno, price))
+
+#You can use index numbers {0} to be sure the arguments are placed in the correct placeholders:
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+print(myorder.format(quantity, itemno, price))
+
+#Escape Character
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt) 
+
+
+"""
+Escape Characters
+\' 	Single Quote 	
+\\ 	Backslash 	
+\n 	New Line 	
+\r 	Carriage Return 	
+\t 	Tab 	
+\b 	Backspace 	
+\f 	Form Feed 	
+\ooo 	Octal value 	
+\xhh 	Hex value
+"""
+
+#Boolean Values
+print(10 > 9)
+print(10 == 9)
+print(10 < 9) 
