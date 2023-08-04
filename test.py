60a2cf094e3c685716cd2989bcd698e8bafcf783
Thendo Charmaine Marageni <132996253+Teddylikeabear@users.noreply.github.com>
2023-08-04 09:01:56 +0200

    9am04Aug

--- test.py
+++ test.py
@@ -4,3 +4,58 @@ b=200
 if b>a :
 
  print ("b is greater than a")
+
+if 5 > 2 :
+  print("Five is greater than two!")
+
+x = 5 
+y = "Hellow world!"
+
+print(x)
+print(y)
+
+#this is how you comment
+
+#creating variables
+
+x = "Teddy"
+y = "Thendo"
+print(x)
+print(y)
+
+#casting
+
+x = str(3)     # x will be '3'
+y = int(3)     # y will be 3
+z = float(3)   # z will be 3.0
+
+#printing the type of class
+x = 5
+y = "Marageni"
+print(type(x))
+print(type(y)) 
+
+#string variables can be declared either by using single or double quotes
+
+#variables are case-sensitive
+
+#variable names
+
+myvar = "T"
+my_var = "E"
+_my_var = "D"
+myVar = "D"
+MYVAR = "Y"
+myvar2 = "BEAR"
+
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
+
+#Camel Case myVariableName = "TED"
+#Pascal Case MyVariableName = "TEDDY"
+#Snake Case my_variable_name = "TEDDYBEAR"
