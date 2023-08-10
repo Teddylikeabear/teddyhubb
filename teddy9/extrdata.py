b7c62767184c7f3794bae10c1306dc27016e9176
Thendo Charmaine Marageni <132996253+Teddylikeabear@users.noreply.github.com>
2023-08-10 09:06:43 +0200

    9am10Aug

---
+++ teddy9/extrdata.py
@@ -0,0 +1,16 @@
+# Python program to illustrate
+# getting input from user
+name = input("Enter your name: ") 
+  
+# user entered the name 'harssh'
+print("hello", name)
+
+msg = "Roll a dice"
+print(msg)
+
+import numpy as np
+
+msg = "Roll a dice"
+print(msg)
+
+print(np.random.randint(1,9))
