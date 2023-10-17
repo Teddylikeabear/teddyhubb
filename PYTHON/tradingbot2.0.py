b6e813091c5b35fac36a2568b2d1ed7f818d131b
Thendo Charmaine Marageni <132996253+Teddylikeabear@users.noreply.github.com>
2023-10-17 15:33:39 +0200

    17/10

---
+++ PYTHON/tradingbot2.0.py
@@ -0,0 +1,64 @@
+import pandas as pd
+
+# Define your strategy here
+
+def trading_strategy(data):
+    # Implement your trading strategy here
+    # This function should return a trading signal (e.g., 'Buy', 'Sell', or '')
+    # based on your analysis of the data.
+
+    # Example: Moving Average Crossover Strategy
+    short_window = 50
+    long_window = 200
+
+    data['SMA_Short'] = data['Close'].rolling(window=short_window).mean()
+    data['SMA_Long'] = data['Close'].rolling(window=long_window).mean()
+
+    if data['SMA_Short'].iloc[-1] > data['SMA_Long'].iloc[-1]:
+        return 'Buy'
+    elif data['SMA_Short'].iloc[-1] < data['SMA_Long'].iloc[-1]:
+        return 'Sell'
+    else:
+        return ''
+
+# Connect to your data source (e.g., obtain historical price data)
+# Replace this with code for accessing your data source (e.g., API calls or CSV file reading).
+# Example:
+data = pd.read_csv('C:\\Users\\charm\\OneDrive\\Desktop\\historical_price_data.csv')
+
+# Implement real-time data streaming and live data updates here (e.g., WebSocket connections).
+
+# Initialize variables for your portfolio
+portfolio = 10000  # Initial capital
+position = 0  # Number of shares held
+current_balance = portfolio
+
+# Implement order execution and interaction with a brokerage's API
+# Replace this section with code for executing orders in a real brokerage.
+# Example:
+def execute_order(symbol, quantity, action):
+    # Place orders using the brokerage's API (e.g., Alpaca, Interactive Brokers).
+    # Handle order placement and error handling.
+    # Update your portfolio and position accordingly.
+
+# Main trading loop
+ for index, row in data.iterrows():
+    signal = trading_strategy(data.loc[:index])
+
+    if signal == 'Buy':
+        shares_to_buy = current_balance // row['Close']
+        execute_order('AAPL', shares_to_buy, 'buy')
+        position += shares_to_buy
+        current_balance -= shares_to_buy * row['Close']
+    elif signal == 'Sell':
+        execute_order('AAPL', position, 'sell')
+        current_balance += position * row['Close']
+        position = 0
+
+# Calculate final portfolio value
+portfolio_value = current_balance + (position * data['Close'].iloc[-1])
+print(f'Final Portfolio Value: {portfolio_value}')
+
+# Implement logging, error handling, and real-time monitoring.
+
+# Consider adding risk management, stop-loss, and take-profit features.
