cdf0904b2be19eff58208c24a8067ed7330ee24b
Teddylikeabear <nyatshiisana@gmail.com>
2023-11-17 12:59:01 +0200

    11/17

---
+++ PYTHON/bootflaskweb/static/css/js/custom.js
@@ -0,0 +1,8 @@
+// static/js/custom.js
+
+// Example JavaScript function for interactivity
+function showAlert() {
+    alert("Hello! This is an example alert.");
+}
+
+// You can add more JavaScript functions and interactivity as needed.
