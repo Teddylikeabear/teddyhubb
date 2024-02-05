65ed2049dc83f3615a22ec63dae3d45799644241
Teddylikeabear <nyatshiisana@gmail.com>
2024-02-05 12:04:59 +0200

    12:04

--- PYTHON/onlineshopping3/app.py
+++ PYTHON/onlineshopping3/app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'Teddylikeabear2703@'
+
 
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
@@ -29,8 +33,29 @@ class CartItem(db.Model):
     quantity = db.Column(db.Integer, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     product = db.relationship('Product', backref='cart_items')
-    user = db.relationship('User', backref='cart_items')   
-    
+    user = db.relationship('User', backref='cart_items')  
+
+class Supplier(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+class SupplierRegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Register as Supplier')
+
+class SupplierLoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login as Supplier')   
+
+class ProductForm(FlaskForm):
+    name = StringField('Product Name', validators=[DataRequired()])
+    description = StringField('Product Description')
+    price = FloatField('Product Price', validators=[DataRequired()])
+    submit = SubmitField('Add Product')    
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -104,6 +129,7 @@ def view_products():
     products = Product.query.all()
     return render_template('view_products.html', products=products)
 
+
 @app.route('/cart')
 def cart():
     cart_items = CartItem.query.filter_by(user_id=current_user.id).all()
@@ -159,6 +185,64 @@ def update_account_settings():
 def signout():
     logout_user()
     return render_template('signout.html')
+
+@app.route('/register_supplier', methods=['GET', 'POST'])
+def register_supplier():
+    form = SupplierRegistrationForm()
+
+    if form.validate_on_submit():
+        
+        new_supplier = Supplier(username=form.username.data, password=form.password.data)
+        db.session.add(new_supplier)
+        db.session.commit()
+        flash('Supplier account created successfully!', 'success')
+        return redirect(url_for('login')) 
+
+    return render_template('register_supplier.html', form=form)
+
+@app.route('/login_supplier', methods=['GET', 'POST'])
+def login_supplier():
+    form = SupplierLoginForm()
+
+    if form.validate_on_submit():
+        supplier = Supplier.query.filter_by(username=form.username.data).first()
+
+        if supplier and supplier.check_password(form.password.data):
+            login_user(supplier)
+            flash('Login successful!', 'success')
+            return redirect(url_for('supplier_dashboard'))
+
+        flash('Invalid username or password. Please try again.', 'danger')
+
+    return render_template('login_supplier.html', form=form)
+
+@app.route('/supplier_dashboard')
+@login_required
+def supplier_dashboard():
+    return render_template('supplier_dashboard.html')
+
+@app.route('/add_product', methods=['GET', 'POST'])
+@login_required
+def add_product():
+    form = ProductForm()
+
+    if form.validate_on_submit():
+       
+        new_product = Product(name=form.name.data, description=form.description.data, price=form.price.data, supplier_id=current_user.id)
+        db.session.add(new_product)
+        db.session.commit()
+        flash('Product added successfully!', 'success')
+        return redirect(url_for('supplier_dashboard'))
+
+    return render_template('add_product.html', form=form)
+
+@app.route('/logout_supplier')
+@login_required
+def logout_supplier():
+    logout_user()
+    return redirect(url_for('index'))
+
+
 
 if __name__ == '__main__':
     with app.app_context():
